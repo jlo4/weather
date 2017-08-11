9d417ac3bf15710205e5aafe7871f8df1617bb79
John Love <john.love4@gmail.com>
2017-08-11 06:45:22 +0300

    remove

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '96f04e24a1beb989576c5b0ce0af7e5e';
+const API_KEY;
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
